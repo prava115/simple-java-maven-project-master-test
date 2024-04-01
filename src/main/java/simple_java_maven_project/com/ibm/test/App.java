33542ef5530f869bedaf6e862c4ff2673d58ea36
Pravakar Kumar <pravakku@in.ibm.com>
2024-04-01 20:18:04 +0530

    Commit

---
+++ src/main/java/simple_java_maven_project/com/ibm/test/App.java
@@ -0,0 +1,13 @@
+package simple_java_maven_project.com.ibm.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
